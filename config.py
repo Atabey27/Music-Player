56e267e917bf64a11e7757f31dd49a1f97916ebd
Atabey27 <48602960+Atabey27@users.noreply.github.com>
2023-07-05 01:07:55 +0300

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("Userbot")
+SESSION_NAME = getenv("BAARhgsArX4-KO0WK6QxTi1NIFdn2QVelz8MxwEvHurGLjcwyQqiE8ifw6uyluR1F0O2L21-2UXQ_i1fBd0VkiqOMugRoZS-Y3H3rSpi2n6nFa3X-kT4voEQoE-plUflJvaSEuN4GX5i8UIwXh-ewNH1KRfIj7O9MxQMaKH_hYBZahS_2Pz_lZlXbxMkG190PpkHMzGTKmVK0FL-q9_rehGLf7bG_81M2N11dClxWhVFFQqcCPXILP7htND7mFn6QzvN52SDZmZpuCf7RYVAdiLhEPb_XZkQ4Q2zq5Th36ZD-SHkorafoqF1i03RiStqsujhAI03m_wiVSvF0QNkngVGKy79zAAAAAF79Q4uAQ")
 BOT_TOKEN = getenv("6374624814:AAEKpIzru9w_ww4Oz5G8fvn7ueA5h1IJWTc")
 BOT_USERNAME = getenv("Muzikcalarxbot")
 admins = {}
