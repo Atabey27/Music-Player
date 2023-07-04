943d13525a0f9fee59bca79fae6d06256b2db8b8
Atabey27 <48602960+Atabey27@users.noreply.github.com>
2023-07-05 01:04:08 +0300

    config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "")
+SESSION_NAME = getenv("Userbot")
+BOT_TOKEN = getenv("6374624814:AAEKpIzru9w_ww4Oz5G8fvn7ueA5h1IJWTc")
+BOT_USERNAME = getenv("Muzikcalarxbot")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("1148427"))
+API_HASH = getenv("362406c73185652edcf9942fde49719c")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("1285704630").split()))
